8424ecf85c66eee18150bd75a8b15a26435c8fb2
Nithish Sankaranarayanan <nithishsankaranarayanan@gmail.com>
2019-12-10 21:34:43 +0100

    first trigger

--- datagen/ingest_postgres.py
+++ datagen/ingest_postgres.py
@@ -8,16 +8,15 @@ def main():
     with psycopg2.connect(host='localhost', port=5432, database='benchmark', user='benchmark',
                           password='benchmark') as conn:
         with conn.cursor() as cur, open('data.txt', 'r') as data:
-            print(int(time.time()))
+            print(int(time.time_ns()))
             counter = 0
             for line in data:
                 cur.execute(sql, (line,))
-                # conn.commit()
                 counter += 1
                 if counter % 100 == 0:
                     conn.commit()
             conn.commit()
-            print(int(time.time()))
+            print(int(time.time_ns()))
 
 
 if __name__ == '__main__':
--- datagen/intidb.sql
+++ datagen/intidb.sql
@@ -1,2 +1,42 @@
 DROP TABLE IF EXISTS events;
-CREATE TABLE events (rsvp json NOT NULL );
+CREATE TABLE events
+(
+    rsvp json NOT NULL
+);
+DROP TABLE IF EXISTS user_count;
+CREATE TABLE user_count
+(
+    member_id   int PRIMARY KEY,
+    member_name VARCHAR,
+    total_rsvps int
+);
+
+DROP TABLE IF EXISTS event_count;
+CREATE TABLE event_count
+(
+    event_id   VARCHAR PRIMARY KEY,
+    event_name VARCHAR,
+    total_rsvp int
+);
+
+CREATE OR REPLACE FUNCTION process_user_rsvp_count() RETURNS TRIGGER AS
+$user_rsvp_count$
+begin
+    if NEW.rsvp ->> 'response' = 'yes' then
+        INSERT INTO user_count
+        VALUES (((NEW.rsvp -> 'member') ->> 'member_id')::int, (NEW.rsvp -> 'member') ->> 'member_name', 1)
+        ON CONFLICT (member_id) DO UPDATE SET total_rsvps = user_count.total_rsvps + 1;
+    end if;
+    RETURN NEW;
+END;
+$user_rsvp_count$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS user_rsvp_count ON events;
+CREATE
+    TRIGGER
+    user_rsvp_count
+    AFTER
+        INSERT
+    ON events
+    FOR EACH ROW
+EXECUTE FUNCTION process_user_rsvp_count();
