54bc4fe5defa66c54b5cb96c497432e77bebe4e9
Nithish Sankaranarayanan <nithishsankaranarayanan@gmail.com>
2019-12-11 16:13:36 +0100

    Time in nanoseconds for ingestion

--- datagen/ingest_kafka.py
+++ datagen/ingest_kafka.py
@@ -1,17 +1,16 @@
-from datetime import datetime
-
 import time
+
 from kafka import KafkaProducer
 
 
 def main():
     producer = KafkaProducer(bootstrap_servers='localhost:9092')
     with open('data.txt', 'r') as data:
-        print(int(time.time()))
+        print(int(time.time_ns()))
         for line in data:
             producer.send(topic='meetup', value=line.encode('utf-8'))
-    producer.flush()
-    print(int(time.time()))
+        producer.flush()
+        print(int(time.time_ns()))
 
 
 if __name__ == '__main__':
